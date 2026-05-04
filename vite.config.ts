28b3eab004ce2d22d1be88d062c3793eedcfc0f6
Kazuya Matsuda <alsterwasser.sg@gmail.com>
2026-05-04 22:30:41 +0900

    fea: 将来的なビルド設定切り替えに備え、設定ファイルを修正した。

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,36 @@ import { defineConfig } from "vite";
 import { viteSingleFile } from "vite-plugin-singlefile";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+export default defineConfig(({ mode }) => {
+  // 環境変数からターゲットを取得（未指定なら index.html）
+  const target = process.env.APP_TARGET || "toybox";
+  const inputHtml = `${target}.html`;
+
+  return {
+    plugins: [viteSingleFile(), tsconfigPaths()],
+    esbuild: {
+      minifyWhitespace: true,
+      minifyIdentifiers: true,
+      minifySyntax: true,
+    },
+    build: {
+      target: "es2020",
+      assetsInlineLimit: 100000000,
+      chunkSizeWarningLimit: 100000000,
+      cssCodeSplit: false,
+      minify: "esbuild",
+      reportCompressedSize: false,
+      rollupOptions: {
+        input: `${inputHtml}`,
+        output: {
+          inlineDynamicImports: true,
+          manualChunks: undefined,
+        },
+      },
+    },
+  };
+});
+/*
 export default defineConfig({
   plugins: [viteSingleFile(), tsconfigPaths()],
   esbuild: {
@@ -25,3 +55,4 @@ export default defineConfig({
     },
   },
 });
+*/
