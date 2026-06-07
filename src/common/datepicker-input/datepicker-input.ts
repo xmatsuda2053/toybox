ad3cd070fcb548dc2b293f73aeb8feb40e416560
Kazuya Matsuda <alsterwasser.sg@gmail.com>
2026-06-07 21:16:21 +0900

    refactor: #83 データピッカーのコードを整理した。

--- src/common/datepicker-input/datepicker-input.ts
+++ src/common/datepicker-input/datepicker-input.ts
@@ -1,22 +1,22 @@
-// 1. Core Libraries
+// Core Libraries
 import {
-  css,
   html,
   LitElement,
   unsafeCSS,
   type HTMLTemplateResult,
   type PropertyValues,
 } from "lit";
+import { map } from "lit/directives/map.js";
 
-// 2. Lit Extensions (Decorators & Directives)
+// Lit Extensions (Decorators & Directives)
 import { customElement, state, property, query } from "lit/decorators.js";
 import { classMap } from "lit/directives/class-map.js";
 
-// 3. Third-party UI & SDKs (WebAwesome)
+// Third-party UI & SDKs (WebAwesome)
 import { setBasePath } from "@awesome.me/webawesome/dist/utilities/base-path.js";
 import WaPopover from "@awesome.me/webawesome/dist/components/popover/popover.js";
 
-// 4. Internal Modules (Database, Models, Shared Components)
+// Internal Modules (Database, Models, Shared Components)
 import {
   formatDate,
   getStartDay,
@@ -26,12 +26,12 @@ import {
 } from "@/utils/DateUtils";
 import { emit } from "@/utils/EventUtils";
 
-// 6. Styles
+// Styles
 import "@awesome.me/webawesome/dist/styles/webawesome.css";
 import sharedStyles from "@shared/shared-css.lit.scss?inline";
 import styles from "./datepicker-input.lit.scss?inline";
 
-// 7. Initializations
+// Initializations
 setBasePath("/");
 
 /**
@@ -42,7 +42,62 @@ type weekName = {
   isHoliday: boolean;
 };
 
-const weekConfig: weekName[] = [
+/**
+ * メニューボタン設定定義
+ */
+type menuButtonConfig = {
+  id: string;
+  label: string;
+  icon: string;
+  type: "month" | "year" | "today";
+  method: "none" | "increment" | "decrement";
+};
+
+/**
+ * メニューボタン設定
+ */
+const BUTTON_CONFIGS: menuButtonConfig[] = [
+  {
+    id: "btn-last-year",
+    label: "前年",
+    icon: "angles-left-solid-full",
+    type: "year",
+    method: "decrement",
+  },
+  {
+    id: "btn-last-month",
+    label: "前月",
+    icon: "angle-left-solid-full",
+    type: "month",
+    method: "decrement",
+  },
+  {
+    id: "btn-today",
+    label: "当日",
+    icon: "location-dot-solid-full",
+    type: "today",
+    method: "none",
+  },
+  {
+    id: "btn-next-month",
+    label: "翌月",
+    icon: "angle-right-solid-full",
+    type: "month",
+    method: "increment",
+  },
+  {
+    id: "btn-next-year",
+    label: "翌年",
+    icon: "angles-right-solid-full",
+    type: "year",
+    method: "increment",
+  },
+];
+
+/**
+ * 曜日名および祝日設定
+ */
+const WEEK_CONFIG: weekName[] = [
   { name: "日", isHoliday: true },
   { name: "月", isHoliday: false },
   { name: "火", isHoliday: false },
@@ -55,10 +110,10 @@ const weekConfig: weekName[] = [
 /**
  * カレンダー描画時の加算日数
  */
-const additionalDays = [...Array(42).keys()] as const;
+const ADDITIONAL_DAYS = [...Array(42).keys()] as const;
 
 /**
- * コピー専用ボタン
+ * データピッカー
  *
  * @export
  * @class DatePickerInput
@@ -78,14 +133,7 @@ export class DatePickerInput extends LitElement {
    * @static
    * @memberof DatePickerInput
    */
-  static styles = [
-    css`
-      ${unsafeCSS(sharedStyles)}
-    `,
-    css`
-      ${unsafeCSS(styles)}
-    `,
-  ];
+  static styles = [unsafeCSS(sharedStyles), unsafeCSS(styles)];
 
   /**
    * 現在選択中の年月
@@ -109,6 +157,13 @@ export class DatePickerInput extends LitElement {
    * @memberof DatePickerInput
    */
   private _value: string = formatDate(new Date(), "yyyy-MM-dd");
+
+  /**
+   * 選択中の日付
+   *
+   * @readonly
+   * @memberof DatePickerInput
+   */
   @property({ type: String })
   get value() {
     return this._value;
@@ -138,14 +193,9 @@ export class DatePickerInput extends LitElement {
    */
   @property({ type: String }) size: "small" | "medium" | "large" = "medium";
 
-  /**
-   * コンポーネント削除時
-   *
-   * @memberof DatePickerInput
-   */
-  disconnectedCallback() {
-    super.disconnectedCallback();
-  }
+  // -------------------------------------------------------------
+  // Lifecycle
+  // -------------------------------------------------------------
 
   /**
    * プロパティ変更時の処理
@@ -164,6 +214,68 @@ export class DatePickerInput extends LitElement {
     }
   }
 
+  // -------------------------------------------------------------
+  // Event
+  // -------------------------------------------------------------
+
+  /**
+   * 指定した日付を選択状態とします。
+   *
+   * @private
+   * @param {Event} e
+   * @memberof DatePickerInput
+   */
+  private _handleDateValueClick = (e: Event) => {
+    const target = e.target as HTMLElement;
+    const dateStr = target.dataset.date;
+    if (!dateStr) return;
+
+    this.value = dateStr;
+    this.calenderPopover.open = false;
+
+    emit(this, "input");
+    emit(this, "change");
+  };
+
+  /**
+   * メニューボタンクリック時の日付計算を制御します。
+   *
+   * @private
+   * @param {Event} e
+   * @memberof DatePickerInput
+   */
+  private _handleMenuButtonClick = (e: Event) => {
+    const target = e.target as HTMLElement;
+    const type = target.dataset.type;
+    const method = target.dataset.method;
+
+    if (!type || !method) return;
+
+    const add = method === "increment" ? 1 : -1;
+    const date = type === "today" ? new Date() : new Date(this.currentMY);
+
+    switch (type) {
+      case "year":
+        date.setFullYear(date.getFullYear() + add);
+        break;
+      case "month":
+        date.setMonth(date.getMonth() + add);
+        break;
+      case "today":
+        this.value = formatDate(date, "yyyy-MM-dd");
+        break;
+      default:
+        this.value = formatDate(date, "yyyy-MM-dd");
+        break;
+    }
+
+    this.currentMY = formatDate(date, "yyyy-MM");
+  };
+
+  // -------------------------------------------------------------
+  // Rendering
+  // -------------------------------------------------------------
+
   /**
    * コンポーネントをレンダリングします。
    *
@@ -171,182 +283,144 @@ export class DatePickerInput extends LitElement {
    * @memberof DatePickerInput
    */
   render(): HTMLTemplateResult {
-    const dateValue = new Date(this.value);
-    const dateCurrent = new Date(this.currentMY);
-
-    const dateCurrentStr = formatDate(dateCurrent, "yyyy年M月");
-    const jpMatch = convertToJapaneseCalendar(dateCurrent).match(/^(.+?[年])/);
-    const jpDateCurrent = jpMatch ? jpMatch[1] : "";
-
-    const formattedDate = formatDate(dateValue, "yyyy-MM-dd (EEE)");
-
-    const startDay = getStartDay(dateCurrent);
-    startDay.setHours(0, 0, 0, 0);
-
-    return html` <wa-input
-        id="input-date"
-        size=${this.size}
-        .value=${formattedDate}
-        readonly
-      >
-        <wa-icon
-          library="my-icons"
-          name="calendar-solid-full"
-          slot="end"
-        ></wa-icon>
-      </wa-input>
+    return html` ${this._renderInput()}
       <wa-popover
         id="calendar-popover"
         for="input-date"
         placement="bottom-start"
       >
         <div class="controller">
-          <div class="ym">${dateCurrentStr}<span>(${jpDateCurrent})</span></div>
-          <div class="btn">
-            <wa-tooltip for="btn-last-year">前年</wa-tooltip>
-            <wa-icon
-              id="btn-last-year"
-              library="my-icons"
-              name="angles-left-solid-full"
-              @click=${() => {
-                this._calcYear(-1);
-              }}
-            ></wa-icon>
-          </div>
-          <div class="btn">
-            <wa-tooltip for="btn-last-month">前月</wa-tooltip>
-            <wa-icon
-              id="btn-last-month"
-              library="my-icons"
-              name="angle-left-solid-full"
-              @click=${() => {
-                this._calcMonth(-1);
-              }}
-            ></wa-icon>
-          </div>
-          <div class="btn">
-            <wa-tooltip for="btn-today">当日</wa-tooltip>
-            <wa-icon
-              id="btn-today"
-              library="my-icons"
-              name="location-dot-solid-full"
-              @click=${() => {
-                this._setToday();
-              }}
-            ></wa-icon>
-          </div>
-          <div class="btn">
-            <wa-tooltip for="btn-next-month">翌月</wa-tooltip>
-            <wa-icon
-              id="btn-next-month"
-              library="my-icons"
-              name="angle-right-solid-full"
-              @click=${() => {
-                this._calcMonth(1);
-              }}
-            ></wa-icon>
-          </div>
-          <wa-tooltip for="btn-next-year">翌年</wa-tooltip>
-          <div class="btn">
-            <wa-icon
-              id="btn-next-year"
-              library="my-icons"
-              name="angles-right-solid-full"
-              @click=${() => {
-                this._calcYear(1);
-              }}
-            ></wa-icon>
-          </div>
+          <!--年月-->
+          ${this._renderCurrentYearMonth()}
+          <!--メニューボタン-->
+          ${this._renderMenuButtons()}
         </div>
         <wa-divider></wa-divider>
         <div class="container">
-          <!--曜日名を出力-->
-          ${weekConfig.map((w) => {
-            const baseClassMap = classMap({
-              cell: true,
-              week: true,
-              holiday: w.isHoliday,
-            });
-            return html`<div class=${baseClassMap}>${w.name}</div>`;
-          })}
-          <!--日付を出力-->
-          ${additionalDays.map((d) => {
-            const date = addDays(startDay, d);
-            const day = formatDate(date, "d");
-
-            const isSysYm = isMatchYM(date, dateCurrent);
-            const isCurrent = formatDate(date, "yyyy-MM-dd") === this.value;
-
-            const baseClassMap = classMap({
-              cell: true,
-              day: true,
-              current: isCurrent,
-              notSysYm: !isSysYm,
-              holiday: weekConfig[date.getDay()].isHoliday,
-            });
-
-            return html`<div
-              class=${baseClassMap}
-              @click=${() => {
-                this._updateValue(date);
-              }}
-            >
-              ${day}
-            </div>`;
-          })}
+          <!--曜日名-->
+          ${this._renderWeekDay()}
+          <!--日付-->
+          ${this._renderDayBit()}
         </div>
       </wa-popover>`;
   }
 
   /**
-   * valueの値を更新します。
+   * 日付入力欄をレンダリングします。
    *
    * @private
-   * @param {Date} date
+   * @return {*}  {HTMLTemplateResult}
    * @memberof DatePickerInput
    */
-  private _updateValue(date: Date) {
-    this.value = formatDate(date, "yyyy-MM-dd");
-    this.calenderPopover.open = false;
-    emit(this, "input");
-    emit(this, "change");
+  private _renderInput(): HTMLTemplateResult {
+    return html`<wa-input
+      id="input-date"
+      size=${this.size}
+      .value=${formatDate(new Date(this.value), "yyyy-MM-dd (EEE)")}
+      readonly
+    >
+      <wa-icon
+        library="my-icons"
+        name="calendar-solid-full"
+        slot="end"
+      ></wa-icon>
+    </wa-input>`;
   }
 
   /**
-   * 選択中の年月の月を加減算します。
+   * 現在年月をレンダリングします。
    *
    * @private
-   * @param {number} addMonth
+   * @return {*}  {HTMLTemplateResult}
    * @memberof DatePickerInput
    */
-  private _calcMonth(addMonth: number) {
-    const date = new Date(this.currentMY);
-    date.setMonth(date.getMonth() + addMonth);
-    this.currentMY = formatDate(date, "yyyy-MM");
+  private _renderCurrentYearMonth(): HTMLTemplateResult {
+    const dateCurrent = new Date(this.currentMY);
+    const dateCurrentStr = formatDate(dateCurrent, "yyyy年M月");
+    const jpMatch = convertToJapaneseCalendar(dateCurrent).match(/^(.+?[年])/);
+    const jpDateCurrent = jpMatch ? jpMatch[1] : "";
+
+    return html`<div class="ym">
+      ${dateCurrentStr}<span>(${jpDateCurrent})</span>
+    </div>`;
   }
 
   /**
-   * 選択中の年月の年を加減算します。
+   * メニューボタンをレンダリングします。
    *
    * @private
-   * @param {number} addYear
+   * @return {*}  {HTMLTemplateResult}
    * @memberof DatePickerInput
    */
-  private _calcYear(addYear: number) {
-    const date = new Date(this.currentMY);
-    date.setFullYear(date.getFullYear() + addYear);
-    this.currentMY = formatDate(date, "yyyy-MM");
+  private _renderMenuButtons(): HTMLTemplateResult {
+    return html`${map(BUTTON_CONFIGS, (config) => {
+      return html` <div class="btn">
+        <wa-tooltip for=${config.id}>${config.label}</wa-tooltip>
+        <wa-icon
+          id=${config.id}
+          library="my-icons"
+          name=${config.icon}
+          data-type=${config.type}
+          data-method=${config.method}
+          @click=${this._handleMenuButtonClick}
+        ></wa-icon>
+      </div>`;
+    })}`;
   }
 
   /**
-   * 当日を選択します。
+   * 曜日名をレンダリングします。
    *
    * @private
+   * @return {*}  {HTMLTemplateResult}
    * @memberof DatePickerInput
    */
-  private _setToday() {
-    const date = new Date();
-    this.value = formatDate(date, "yyyy-MM-dd");
-    this.currentMY = formatDate(date, "yyyy-MM");
+  private _renderWeekDay(): HTMLTemplateResult {
+    return html` ${map(WEEK_CONFIG, (w) => {
+      return html`<div
+        class=${classMap({
+          cell: true,
+          week: true,
+          holiday: w.isHoliday,
+        })}
+      >
+        ${w.name}
+      </div>`;
+    })}`;
+  }
+
+  /**
+   * 日付をレンダリングします。
+   *
+   * @private
+   * @return {*}  {HTMLTemplateResult}
+   * @memberof DatePickerInput
+   */
+  private _renderDayBit(): HTMLTemplateResult {
+    const dateCurrent = new Date(this.currentMY);
+    const startDay = getStartDay(dateCurrent);
+    startDay.setHours(0, 0, 0, 0);
+
+    return html` ${map(ADDITIONAL_DAYS, (d) => {
+      const date = addDays(startDay, d);
+      const dateStr = formatDate(date, "yyyy-MM-dd");
+
+      const baseClassMap = classMap({
+        cell: true,
+        day: true,
+        current: dateStr === this.value,
+        today: dateStr === formatDate(new Date(), "yyyy-MM-dd"),
+        notSysYm: !isMatchYM(date, dateCurrent),
+        holiday: WEEK_CONFIG[date.getDay()].isHoliday,
+      });
+
+      return html`<div
+        class=${baseClassMap}
+        data-date=${dateStr}
+        @click=${this._handleDateValueClick}
+      >
+        ${formatDate(date, "d")}
+      </div>`;
+    })}`;
   }
 }
