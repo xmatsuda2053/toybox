724105f88280d50e7e2335beb88b544a5ebeb94e
Kazuya Matsuda <alsterwasser.sg@gmail.com>
2026-06-10 23:31:20 +0900

    feature: #99 Step-Noteに新機能を実装するための準備を行った。

--- src/apps/stepnote/step-note-app.ts
+++ src/apps/stepnote/step-note-app.ts
@@ -1,23 +1,24 @@
-// 1. Core Libraries
+// Core Libraries
 import {
-  css,
   html,
   LitElement,
   unsafeCSS,
   type HTMLTemplateResult,
-  type PropertyValues,
+  nothing,
 } from "lit";
+import { classMap } from "lit/directives/class-map.js";
 
-// 2. Decorators & Directives
+// Decorators & Directives
 import { customElement, state } from "lit/decorators.js";
 
-// 3. Third-party Components & Utils
+// Third-party Components & Utils
 import { setBasePath } from "@awesome.me/webawesome/dist/utilities/base-path.js";
 
-// 4. Internal Modules (Database, Models, Shared Components)
+// Internal Modules (Database, Models, Shared Components)
 import { snDB } from "@sn/database/SnDB";
+import { features } from "./components/menu/sn-menu";
 
-// 5. Styles
+// Styles
 import "@awesome.me/webawesome/dist/styles/webawesome.css";
 import styles from "@sn/styles/step-note-app.lit.scss?inline";
 
@@ -35,59 +36,76 @@ export class StepNoteApp extends LitElement {
   @state() isThinMode: boolean = false;
 
   /**
+   * 選択中の機能
+   *
+   * @type {features}
+   * @memberof StepNoteApp
+   */
+  @state() selectedFeature: features = "main";
+
+  /**
    * スタイルシートを適用
    *
    * @static
    * @memberof StepNoteApp
    */
-  static styles = css`
-    ${unsafeCSS(styles)}
-  `;
+  static styles = [unsafeCSS(styles)];
+
+  // -------------------------------------------------------------
+  // Event
+  // -------------------------------------------------------------
 
   /**
-   * ショートカットキー
+   * タスクナビゲーションを表示する
    *
    * @private
-   * @param {KeyboardEvent} e
-   * @memberof SnList
+   * @memberof StepNoteApp
    */
-  private _shortcutKey = (e: KeyboardEvent) => {
-    if (e.altKey && e.shiftKey && (e.key === "E" || e.key === "e")) {
-      this._toggleExploreNav();
+  private _handleMainClick = () => {
+    if (this.selectedFeature === "main") {
+      this.isThinMode = !this.isThinMode;
+    } else {
+      this.selectedFeature = "main";
+      this.isThinMode = false;
     }
   };
 
   /**
-   * Creates an instance of StepNoteApp.
+   * ダッシュボード画面を表示する
+   *
+   * @private
    * @memberof StepNoteApp
    */
-  constructor() {
-    super();
-    window.addEventListener("keydown", this._shortcutKey);
-  }
+  private _handleDashboardClick = () => {
+    this.selectedFeature = "dashboard";
+    this.isThinMode = true;
+  };
 
   /**
-   * コンポーネントがドキュメントの DOM から削除されたときに実行されます。
+   * 設定画面を表示する
    *
-   * @override
-   * @memberof SnList
-   */
-  disconnectedCallback() {
-    super.disconnectedCallback();
-    window.removeEventListener("keydown", this._shortcutKey);
-  }
-
-  /**
-   * render直前に実行されます。
-   *
-   * @protected
-   * @param {PropertyValues} _changedProperties
+   * @private
    * @memberof StepNoteApp
    */
-  protected willUpdate(_changedProperties: PropertyValues) {
-    super.willUpdate(_changedProperties);
-  }
+  private _handleConfigClick = () => {
+    this.selectedFeature = "config";
+    this.isThinMode = true;
+  };
 
+  /**
+   * 指定されたタスクを選択状態とする。
+   *
+   * @private
+   * @param {CustomEvent} e
+   * @memberof StepNoteApp
+   */
+  private _handleTaskClick = async (e: CustomEvent) => {
+    await snDB.taskRepo.changeTaskAndNavSelection(Number(e.detail.id));
+  };
+
+  // -------------------------------------------------------------
+  // Rendering
+  // -------------------------------------------------------------
   /**
    * コンポーネントのメインレイアウトをレンダリングします。
    * アプリケーションの基本構造を定義します。
@@ -98,63 +116,142 @@ export class StepNoteApp extends LitElement {
    * @memberof StepNoteApp
    */
   protected render(): HTMLTemplateResult {
-    return html`<div id="contents-root">
-      <div
-        class="content ${this.isThinMode ? "thin" : ""}"
-        @id-click=${(e: CustomEvent) => this._taskSelect(e.detail.id)}
+    return html`<div id="contents-root">${this._renderContents()}</div>
+      ${this._renderImportCompleteDialog()}`;
+  }
+
+  /**
+   * メニューバーをレンダリングします。
+   *
+   * @private
+   * @return {*}  {HTMLTemplateResult}
+   * @memberof StepNoteApp
+   */
+  private _renderMenu(): HTMLTemplateResult {
+    return html` <div class="panel menu">
+      <sn-menu
+        @click-menu-main=${this._handleMainClick}
+        @click-menu-dashboard=${this._handleDashboardClick}
+        @click-menu-config=${this._handleConfigClick}
+      ></sn-menu>
+    </div>`;
+  }
+
+  /**
+   * インポート完了ダイアログをレンダリングします。
+   *
+   * @private
+   * @return {*}  {HTMLTemplateResult}
+   * @memberof StepNoteApp
+   */
+  private _renderImportCompleteDialog(): HTMLTemplateResult {
+    return html`<wa-dialog
+      label="Complete"
+      id="dialog-import-complete-overview"
+    >
+      インポート処理が完了しました。
+      <wa-button
+        appearance="filled"
+        slot="footer"
+        variant="brand"
+        data-dialog="close"
       >
-        <div class="panel menu">
-          <sn-menu @click-menu-explore=${this._toggleExploreNav}></sn-menu>
+        Close
+      </wa-button>
+    </wa-dialog>`;
+  }
+
+  /**
+   * コンテンツのレンダリングを制御します。
+   *
+   * @private
+   * @return {*}  {HTMLTemplateResult}
+   * @memberof StepNoteApp
+   */
+  private _renderContents(): HTMLTemplateResult | typeof nothing {
+    switch (this.selectedFeature) {
+      case "main":
+        return this._renderMain();
+      case "dashboard":
+        return this._renderDashboard();
+      case "config":
+        return this._renderConfig();
+      default:
+        return nothing;
+    }
+  }
+
+  /**
+   * メイン機能をレンダリングします。
+   *
+   * @private
+   * @return {*}  {HTMLTemplateResult}
+   * @memberof StepNoteApp
+   */
+  private _renderMain(): HTMLTemplateResult {
+    const baseClassMap = classMap({
+      base: true,
+      "main-content": true,
+      thin: this.isThinMode,
+    });
+
+    return html` <div class=${baseClassMap} @id-click=${this._handleTaskClick}>
+      ${this._renderMenu()}
+      <div class="panel nav">
+        <div class="quick">
+          <sn-nav-section-quick></sn-nav-section-quick>
         </div>
-        <div class="panel nav">
-          <div class="quick">
-            <sn-nav-section-quick></sn-nav-section-quick>
-          </div>
-          <div class="label">
-            <sn-nav-section-label></sn-nav-section-label>
-          </div>
-        </div>
-        <div class="panel list">
-          <sn-list></sn-list>
-        </div>
-        <div class="panel task">
-          <sn-tab-task></sn-tab-task>
-        </div>
-        <div class="panel journal">
-          <sn-tab-journal></sn-tab-journal>
+        <div class="label">
+          <sn-nav-section-label></sn-nav-section-label>
         </div>
       </div>
-      <wa-dialog label="Complete" id="dialog-import-complete-overview">
-        インポート処理が完了しました。
-        <wa-button
-          appearance="filled"
-          slot="footer"
-          variant="brand"
-          data-dialog="close"
-          >Close</wa-button
-        >
-      </wa-dialog>
-    </div> `;
+      <div class="panel list">
+        <sn-list></sn-list>
+      </div>
+      <div class="panel task">
+        <sn-tab-task></sn-tab-task>
+      </div>
+      <div class="panel journal">
+        <sn-tab-journal></sn-tab-journal>
+      </div>
+    </div>`;
   }
 
   /**
-   * タスクナビゲーションを表示する
+   * ダッシュボード画面をレンダリングします。
    *
    * @private
+   * @return {*}  {HTMLTemplateResult}
    * @memberof StepNoteApp
    */
-  private _toggleExploreNav() {
-    this.isThinMode = !this.isThinMode;
+  private _renderDashboard(): HTMLTemplateResult {
+    const baseClassMap = classMap({
+      base: true,
+      "dashboard-content": true,
+    });
+
+    return html` <div class=${baseClassMap}>
+      ${this._renderMenu()}
+      <div>dashboard</div>
+    </div>`;
   }
 
   /**
-   * 指定されたタスクを選択状態とする。
+   * 設定画面をレンダリングします。
    *
    * @private
-   * @param {CustomEvent} e
+   * @return {*}  {HTMLTemplateResult}
    * @memberof StepNoteApp
    */
-  private async _taskSelect(id: number) {
-    await snDB.taskRepo.changeTaskAndNavSelection(Number(id));
+  private _renderConfig(): HTMLTemplateResult {
+    const baseClassMap = classMap({
+      base: true,
+      "config-content": true,
+    });
+
+    return html` <div class=${baseClassMap}>
+      ${this._renderMenu()}
+      <div>config</div>
+    </div>`;
   }
 }
