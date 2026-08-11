6cc80c828d12f9aa4a8e8c90e55b7a89cd41f5ae
Kazuya Matsuda <alsterwasser.sg@gmail.com>
2026-08-11 20:59:29 +0900

    feat: #148 thin-markdown-editor に TextCopyArea 拡張機能を追加

---
+++ src/common/thin-markdown-editor/extension/text-copy-area.ts
@@ -0,0 +1,66 @@
+import { TokenizerAndRendererExtension, Tokens } from "marked";
+
+/**
+ * TextCopyAreaのトークンインターフェース
+ */
+export interface TextCopyAreaToken extends Tokens.Generic {
+  type: "TextCopyArea";
+  raw: string;
+  text: string;
+}
+
+/**
+ * marked.js用のTextCopyAreaタグ拡張機能
+ */
+export const TextCopyAreaExtension: TokenizerAndRendererExtension = {
+  name: "TextCopyArea",
+  level: "block",
+  start(text: string) {
+    return text.indexOf("+++");
+  },
+  tokenizer(text: string): TextCopyAreaToken | undefined {
+    const match = /^(\+{3,})\s*\n([\s\S]*?)\n\1(?:\n|$)/.exec(text);
+    if (match) {
+      return {
+        type: "TextCopyArea",
+        raw: match[0],
+        text: match[2],
+      };
+    }
+    return undefined;
+  },
+  renderer(token: Tokens.Generic): string {
+    const t = token as TextCopyAreaToken;
+    const icon = `<div class="icon"><wa-icon library="my-icons" name="clipboard-regular-full"></wa-icon></div>`;
+    return `<div class="text-copy-area" data-text="${t.text}"><div class="content">${t.text}</div>${icon}</div>`;
+  },
+};
+
+/**
+ * markdown用のフォーマットを適用する
+ * @param textarea
+ */
+export const formatMarkdown = (textarea: HTMLTextAreaElement): void => {
+  // 選択範囲の位置情報を取得
+  const start = textarea.selectionStart;
+  const end = textarea.selectionEnd;
+  const oldText = textarea.value;
+
+  // 選択されたテキストを抽出
+  const selectedText = oldText.substring(start, end);
+
+  // 新しい文字列を作成
+  const textStart = `+++\n`;
+  const textEnd = "\n+++";
+  const newText =
+    oldText.substring(0, start) +
+    textStart +
+    selectedText +
+    textEnd +
+    oldText.substring(end);
+  textarea.value = newText;
+
+  // カーソル位置を選択されたテキストの末尾（textEndの直前）に設定
+  const newCursorPos = start + textStart.length + selectedText.length;
+  textarea.selectionStart = textarea.selectionEnd = newCursorPos;
+};
