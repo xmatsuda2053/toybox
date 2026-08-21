7ff77f88333efc7bee0e6c2a40401722d953f77e
Kazuya Matsuda <alsterwasser.sg@gmail.com>
2026-08-21 23:21:25 +0900

    change: #174 期限フィルタに件数を表示するよう仕様変更

--- src/apps/stepnote/database/calculators/TaskStatsCalculator.ts
+++ src/apps/stepnote/database/calculators/TaskStatsCalculator.ts
@@ -22,7 +22,7 @@ export class TaskStatsCalculator {
    * @return {*}  {Promise<number>}
    * @memberof TaskStatsCalculator
    */
-  async countBookmark(): Promise<number> {
+  async getBookmarkCount(): Promise<number> {
     return await this.db.tasks
       .where("statusCode")
       .anyOf([TaskStatus.PENDING.code, TaskStatus.PROGRESS.code]) // 開始待ち,対応中
@@ -36,7 +36,7 @@ export class TaskStatsCalculator {
    * @return {*}  {Promise<number>}
    * @memberof TaskStatsCalculator
    */
-  async countUncategorized(): Promise<number> {
+  async getUncategorizedCount(): Promise<number> {
     const labels = await this.db.labelRepo.getLabelsAscName();
     const existingLabelSet = new Set(labels.map((l) => l.id));
 
@@ -48,54 +48,48 @@ export class TaskStatsCalculator {
   }
 
   /**
-   * 期限切れタスクの有無を判定します。
+   * 期限切れタスクの件数を取得します。
    *
-   * @return {*}  {Promise<boolean>}
+   * @return {*}  {Promise<number>}
    * @memberof TaskStatsCalculator
    */
-  async hasOverdue(): Promise<boolean> {
+  async getOverdueCount(): Promise<number> {
     const today = new Date();
     today.setHours(0, 0, 0, 0);
 
-    return (
-      (await this.db.tasks
-        .where("statusCode")
-        .anyOf([TaskStatus.PENDING.code, TaskStatus.PROGRESS.code]) // 開始待ち,対応中
-        .filter((task) => task.dueDate < today) // 当日(00:00)より前
-        .count()) > 0
-    );
+    return await this.db.tasks
+      .where("statusCode")
+      .anyOf([TaskStatus.PENDING.code, TaskStatus.PROGRESS.code]) // 開始待ち,対応中
+      .filter((task) => task.dueDate < today) // 当日(00:00)より前
+      .count();
   }
 
   /**
-   * 期限当日タスクの有無を判定します。
+   * 期限当日タスクの件数を取得します。
    * ※As Soon As Possible
    *
-   * @return {*}  {Promise<boolean>}
+   * @return {*}  {Promise<number>}
    * @memberof TaskStatsCalculator
    */
-  async hasAsap(): Promise<boolean> {
-    return (
-      (await this.db.tasks
-        .where("statusCode")
-        .anyOf([TaskStatus.PENDING.code, TaskStatus.PROGRESS.code]) // 開始待ち,対応中
-        .filter((task) => isAsap(false, task.dueDate)) // 当日
-        .count()) > 0
-    );
+  async getAsapCount(): Promise<number> {
+    return await this.db.tasks
+      .where("statusCode")
+      .anyOf([TaskStatus.PENDING.code, TaskStatus.PROGRESS.code]) // 開始待ち,対応中
+      .filter((task) => isAsap(false, task.dueDate)) // 当日
+      .count();
   }
 
   /**
-   * 期限間近タスクの有無を判定します。
+   * 期限間近タスクの件数を取得します。
    *
-   * @return {*}  {Promise<boolean>}
+   * @return {*}  {Promise<number>}
    * @memberof TaskStatsCalculator
    */
-  async hasUpcoming(): Promise<boolean> {
-    return (
-      (await this.db.tasks
-        .where("statusCode")
-        .anyOf([TaskStatus.PENDING.code, TaskStatus.PROGRESS.code]) // 開始待ち,対応中
-        .filter((task) => isWithinAnyDaysBefore(false, task.dueDate)) // 当日(00:00)より前
-        .count()) > 0
-    );
+  async getUpcomingCount(): Promise<number> {
+    return await this.db.tasks
+      .where("statusCode")
+      .anyOf([TaskStatus.PENDING.code, TaskStatus.PROGRESS.code]) // 開始待ち,対応中
+      .filter((task) => isWithinAnyDaysBefore(false, task.dueDate)) // 当日(00:00)より前
+      .count();
   }
 }
